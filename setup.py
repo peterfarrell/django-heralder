def7dfb8ce4f9faeb8b91c57359b29d63be8177f
Robert Roskam <raiderrobert@gmail.com>
2016-07-19 14:46:51 -0400

    Filled out the setup file a bit more

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
 
+VERSION = '0.1'
+
 setup(
     name='django-herald',
-    version='0.1',
+    version=VERSION,
     author='Worthwhile',
     author_email='devs@worthwhile.com',
     packages=find_packages(),
     include_package_data=True,  # declarations in MANIFEST.in
     license='LICENSE',
+    url = 'https://github.com/worthwhile/django-herald/
+    download_url = 'https://github.com/worthwhile/django-herald/tarball/'+VERSION
     description="Django library for separating the message content from transmission method",
     long_description=open('README.md').read(),
+    keywords = ['django', 'notifications', 'messaging'],
 )
