0d50e39803db371c9352bfc5f1c0c853ce647006
Jared Proffitt <jproffitt@worthwhile.com>
2016-07-22 11:10:34 -0500

    fix pypi build

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
-from setuptools import find_packages
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 
-VERSION = '0.1'
+VERSION = '0.1.1'
 
 setup(
     name='django-herald',
@@ -12,10 +11,21 @@ setup(
     install_requires=['django>=1.8', 'six'],
     packages=find_packages(),
     include_package_data=True,  # declarations in MANIFEST.in
-    license='LICENSE',
+    license='MIT',
     url='https://github.com/worthwhile/django-herald/',
     download_url='https://github.com/worthwhile/django-herald/tarball/'+VERSION,
     description="Django library for separating the message content from transmission method",
     long_description=open('README.md').read(),
     keywords=['django', 'notifications', 'messaging'],
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
