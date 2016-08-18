1b2bcc6309d3498f2b2ce4d552a9b0648bde18aa
rroskam <rroskam@worthwhile.com>
2016-08-18 17:02:57 -0400

    Added in tests

---
+++ tests/test_commands.py
@@ -0,0 +1,49 @@
+"""
+Testing custom commands
+"""
+
+from datetime import timedelta
+
+from django.core.management import call_command
+from django.test import TestCase
+from django.utils import timezone
+from django.utils.six import StringIO
+
+from herald.models import SentNotification
+
+
+MSG = 'Successfully deleted {num} notification(s)'
+
+
+class DeleteNotificationNoArgs(TestCase):
+
+    def test_delete_today(self):
+        SentNotification(
+            notification_class='tests.notifications.MyNotification',
+            date_sent=timezone.now()
+        ).save()
+        SentNotification(
+            notification_class='tests.notifications.MyNotification',
+            date_sent=timezone.now()+timedelta(days=1)
+        ).save()
+        out = StringIO()
+        call_command('delnotifs', stdout=out)
+        self.assertIn(MSG.format(num=1), out.getvalue())
+
+    def test_do_not_delete_tomorrow(self):
+        SentNotification(
+            notification_class='tests.notifications.MyNotification',
+            date_sent=timezone.now()+timedelta(days=1)
+        ).save()
+        out = StringIO()
+        call_command('delnotifs', stdout=out)
+        self.assertIn(MSG.format(num=0), out.getvalue())
+
+    def test_do_not_delete_yesterday(self):
+        SentNotification(
+            notification_class='tests.notifications.MyNotification',
+            date_sent=timezone.now() - timedelta(days=1)
+        ).save()
+        out = StringIO()
+        call_command('delnotifs', stdout=out)
+        self.assertIn(MSG.format(num=0), out.getvalue())
