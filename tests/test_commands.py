fd700067c1b30a389950fce7486c734db0cbc6f5
rroskam <rroskam@worthwhile.com>
2016-08-19 10:58:05 -0400

    Improved test coverage by testing the other args and bad params

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -5,6 +5,7 @@ Testing custom commands
 from datetime import timedelta
 
 from django.core.management import call_command
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from django.utils import timezone
 from django.utils.six import StringIO
@@ -52,7 +53,7 @@ class DeleteNotificationNoArgs(TestCase):
 
 class DeleteNotificationArgs(TestCase):
 
-    def test_delete_today(self):
+    def test_delete_today_start_arg(self):
         SentNotification(
             notification_class=NOTIFICATION_CLASS,
             date_sent=timezone.now()
@@ -66,20 +67,25 @@ class DeleteNotificationArgs(TestCase):
         call_command('delnotifs', stdout=out, start=str(today))
         self.assertIn(MSG.format(num=1), out.getvalue())
 
-    def test_do_not_delete_tomorrow(self):
+    def test_do_not_delete_tomorrow_end_arg(self):
         SentNotification(
             notification_class=NOTIFICATION_CLASS,
             date_sent=timezone.now()+timedelta(days=1)
         ).save()
         out = StringIO()
-        call_command('delnotifs', stdout=out, start=str(timezone.now().today()))
-        self.assertIn(MSG.format(num=0), out.getvalue())
+        two_days_later = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=2)
+        call_command('delnotifs', stdout=out, end=str(two_days_later))
+        self.assertIn(MSG.format(num=1), out.getvalue())
 
-    def test_do_not_delete_yesterday(self):
+    def test_do_accept_bad_args(self):
         SentNotification(
             notification_class=NOTIFICATION_CLASS,
             date_sent=timezone.now() - timedelta(days=1)
         ).save()
         out = StringIO()
-        call_command('delnotifs', stdout=out, start=str(timezone.now().today()))
-        self.assertIn(MSG.format(num=0), out.getvalue())
+
+        with self.assertRaises(ValidationError):
+            call_command('delnotifs', stdout=out, start='blargh')
+
+        with self.assertRaises(ValidationError):
+            call_command('delnotifs', stdout=out, start='01-01-2016')
