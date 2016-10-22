b0abd74a5facb3fe6660e4bf073e4b9264c1d91d
Robert Roskam <raiderrobert@gmail.com>
2016-10-21 23:46:54 -0400

    Eliminated the enumerate function call
    Not using the index. So eliminating it.

--- herald/apps.py
+++ herald/apps.py
@@ -21,7 +21,7 @@ class HeraldConfig(AppConfig):
 
         try:
             # add any new notifications to database.
-            for index, klass in enumerate(registry._registry):
+            for klass in registry._registry:
                 Notification.objects.get_or_create(notification_class=klass.__name__)
         except OperationalError:
             # if the table is not created yet, just keep going.
