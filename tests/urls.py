1c630d46466350da20e42d2db1be457d8023d50e
Jared Proffitt <jproffitt@worthwhile.com>
2017-04-03 09:35:51 -0500

    compat fixes

--- tests/urls.py
+++ tests/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls import url, include
 
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url('^', include(auth_urls)),
     url(r'^herald/', include('herald.urls')),
 ]
