4f60a64155739fdfac9739fe5ec3b5373eb5d076
Jordan Koncz <jordanmkoncz@gmail.com>
2018-05-17 08:56:42 +1000

    Change Notification.notification_class and Notification.verbose_name fields to have less restrictive `max_length`

--- herald/models.py
+++ herald/models.py
@@ -82,8 +82,8 @@ class Notification(models.Model):
     """
     NotificationClasses are created on app init.
     """
-    notification_class = models.CharField(max_length=80, unique=True)
-    verbose_name = models.CharField(max_length=100, blank=True, null=True)
+    notification_class = models.CharField(max_length=255, unique=True)
+    verbose_name = models.CharField(max_length=255, blank=True, null=True)
     can_disable = models.BooleanField(default=True)
 
     def __str__(self):
