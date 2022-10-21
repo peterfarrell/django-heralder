b63b0143caec6f56442f8f484d476ded8e7c401a
Josiah White <jwhite@iteris.com>
2022-10-21 11:00:57 -0700

    #6 - Using standard Django settings instead.

--- herald/utils.py
+++ herald/utils.py
@@ -1,17 +1,21 @@
 from django.apps import apps as django_apps
 from django.core.exceptions import ImproperlyConfigured
-
-from herald.conf import settings as herald_settings
+from django.conf import settings
 
 
 def get_sent_notification_model():
     try:
-        return django_apps.get_model(herald_settings.SENT_NOTIFICATION_MODEL)
+        sent_notification = getattr(
+            settings,
+            "HERALD_SENT_NOTIFICATION_MODEL",
+            'herald.SentNotification'
+        )
+        return django_apps.get_model(sent_notification)
     except ValueError:
         raise ImproperlyConfigured(
             "SENT_NOTIFICATION_MODEL must be of the form 'app_label.model_name'"
         )
     except LookupError:
         raise ImproperlyConfigured(
-            f"SENT_NOTIFICATION_MODEL refers to model '{herald_settings.SENT_NOTIFICATION_MODEL}' that has not been installed"
+            f"SENT_NOTIFICATION_MODEL refers to model 'herald.SentNotification' that has not been installed"
         )
