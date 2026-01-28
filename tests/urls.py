8bf8d031eaf72f5c5aa4d6b47626028e0afc2faf
Farrell, Peter (SAA) <peter_farrell@saa.senate.gov>
2026-01-28 09:08:42 -0800

    fix black

--- tests/urls.py
+++ tests/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.contrib.auth import urls as auth_urls
 from django.urls import include, re_path
 
-
 urlpatterns = [
     re_path(r"^admin/", admin.site.urls),
     re_path("^", include(auth_urls)),
